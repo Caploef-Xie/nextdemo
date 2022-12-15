7a4ea95bd767608a2631743d3192dce2484572db
Jinmiao.Xie <xjmsvip@qq.com>
2022-12-15 23:27:38 +0800

    test

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/contracts' },
+      '/contracts': { page: '/contracts' },
+    }
+  },
 }
 
 module.exports = nextConfig
